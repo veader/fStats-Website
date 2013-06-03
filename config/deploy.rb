88c87cc599463473826c2e53fbb8dbaa6d31930d
Shawn Veader <shawn@veader.org>
2013-06-03 00:16:11 -0400

    adding capistrano support

---
+++ config/deploy.rb
@@ -0,0 +1,27 @@
+set :application,   'fstats'
+set :scm,           'git'
+set :repository,    "git@code.veader.org:fStats_Website"
+set :deploy_to,     "/var/www/#{application}"
+set :user,          'web'
+set :keep_releases, 3
+set :use_sudo,      false
+set :deploy_via,    :copy
+set :port,          22
+
+role :web, "fstatsapp.com"
+
+deploy.task :restart, :roles => :web do
+  # DO NOTHING
+end
+
+deploy.task :finalize_update, :except => { :no_release => true } do
+  # DO NOTHING
+end
+
+# namespace :deploy do
+#   task :start do ; end
+#   task :stop do ; end
+#   task :restart, :roles => :app, :except => { :no_release => true } do
+#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+#   end
+# end
